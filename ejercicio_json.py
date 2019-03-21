8e932e4d221954a258427ae007f6002f03f4179d
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-21 12:40:43 +0100

    ejercicio 1 hecho iniciando el 2

--- ejercicio_json.py
+++ ejercicio_json.py
@@ -4,6 +4,22 @@
     #4.- Pedir la vida base(hp) de los campeones por teclado y mostrar a los campeones que una vida igual o superior.
     #5.- Compara algun stat(pedido por teclado) entre dos campeones (pedidos por teclado).
 
-    import json
-with open("leagueoflegends.json") as fichero:
-datos=json.load(fichero)
+
+def listado(datos):
+    lista = []
+    for i in range(0,23):
+        lista.append(datos["Campeones"]["Champion"][i]["name"])
+    return lista
+
+def contar():
+
+
+import json
+
+with open("leagueoflegends.json","r") as fichero:
+    datos = json.load(fichero)
+    
+print("\n1.- Lista el nombre de todos los campeones.\n")
+print(listado(datos))  
+
+print("\n2.- Contar los campeones que sean magos/tanques/luchadores(Lo que pidas por teclado).\n")
